e68052fe2ea954b5ffdea18617cdfbb8bd79f9f8
Sey <76843977+iseykim@users.noreply.github.com>
2021-12-21 18:14:27 -0500

    Squashed 'problem-solving-patterns/' changes from 4c76a33..3e61c51
    3e61c51 add: solutions to problem sets
    2ec60dc add: minSubarrayLen problem
    3d373ec add: algo problem sets
    
    git-subtree-dir: problem-solving-patterns
    git-subtree-split: 3e61c51eb6cd54d615b7cc4a28a649a87a26758a

---
+++ problem-sets/sliding-window/minSubarrayLen.js
@@ -0,0 +1,12 @@
+// Write a function called minSubarrayLen which accepts two parameters - an array of positive integers and a positive integer.
+
+// This function should return the minimal length of a continguous subarray of which the sum is greater than or equal to the integer passed to the function. If there isn't one, return 0 instead.
+
+const minSubarrayLen = (arr, sum) => {
+  // Your code here
+}
+
+// Examples:
+minSubarrayLen([2,3,1,2,4,3], 7) // 2 -> [4,3]
+minSubarrayLen([2,1,6,5,4], 9) // 2 -> [5,4]
+minSubarrayLen([3,1,11,7,62,33]) // 1 -> [62]
