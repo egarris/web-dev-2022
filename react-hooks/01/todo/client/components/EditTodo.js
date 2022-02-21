d8e3576cf3d93e07a85c4a571d0912c515a9bf16
Evan Garris <egarris@gmail.com>
2022-02-21 16:45:10 -0600

    refactor: edittodo using hooks

--- react-hooks/01/todo/client/components/EditTodo.js
+++ react-hooks/01/todo/client/components/EditTodo.js
@@ -1,86 +1,55 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { deleteTodo, updateTodo } from '../store/todos';
 import { fetchTodo, _setTodo } from '../store/todo';
-import { connect } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 
-class EditTodo extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      taskName: '',
-      assignee: ''
-    };
+const EditTodo = () => {
+  const [taskName, setTaskName] = useState('')
+  const [assignee, setAssignee] = useState('')
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  componentDidMount() {
-    const { id } = this.props.match.params;
-    this.props.fetchTodo(id);
-  }
-
-  componentWillUnmount() {
-    this.props.clearTodo();
-  }
-
-  componentDidUpdate(prevProps) {
-    if (prevProps.todo.id !== this.props.todo.id) {
-      this.setState({
-        taskName: this.props.todo.taskName || '',
-        assignee: this.props.todo.assignee || ''
-      });
+  const { todo } = useSelector((state) => {
+    return {
+      todo: state.todo
     }
+  })
+
+  const dispatch = useDispatch()
+  const { id } = useParams()
+
+  useEffect(() => {
+    dispatch(fetchTodo(id))
+  },[])
+
+  useEffect(() => {
+    setTaskName(todo.taskName)
+    setAssignee(todo.assignee)
+  },[todo])
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault
+    dispatch(updateTodo({...todo, taskName, assignee}))
   }
 
-  handleChange(evt) {
-    this.setState({
-      [evt.target.name]: evt.target.value
-    });
-  }
-
-  handleSubmit(evt) {
-    evt.preventDefault();
-    this.props.updateTodo({ ...this.props.todo, ...this.state });
-  }
-
-  render() {
-    const { assignee, taskName } = this.state;
-    const { handleSubmit, handleChange } = this;
-
-    return (
-      <div>
+  return (
+    <div>
         <form id='todo-form' onSubmit={handleSubmit}>
           <label htmlFor='taskName'>Task Name:</label>
-          <input name='taskName' onChange={handleChange} value={taskName} />
+          <input name='taskName' onChange={(e) => setTaskName(e.target.value)} value={taskName || ''} />
 
           <label htmlFor='assignee'>Assign To:</label>
-          <input name='assignee' onChange={handleChange} value={assignee} />
+          <input name='assignee' onChange={(e) => setAssignee(e.target.value)} value={assignee || ''} />
 
           <button type='submit'>Submit</button>
         </form>
         <form onSubmit={(ev) => ev.preventDefault()}>
           <button
             className='remove'
-            onClick={() => this.props.deleteTodo(this.props.match.params.id)}
+            onClick={() => dispatch(deleteTodo(id))}
           >
             Delete
           </button>
         </form>
       </div>
-    );
-  }
+  )
 }
-
-const mapStateToProps = ({ todo }) => ({
-  todo
-});
-
-const mapDispatchToProps = (dispatch, { history }) => ({
-  updateTodo: (todo) => dispatch(updateTodo(todo, history)),
-  deleteTodo: (todo) => dispatch(deleteTodo(todo, history)),
-  fetchTodo: (id) => dispatch(fetchTodo(id)),
-  clearTodo: () => dispatch(_setTodo({}))
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(EditTodo);
